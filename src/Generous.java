f1b073063871bc1e608238e0f74042db299f3c4f
naruapon <naruapon@webmail.npru.ac.th>
2022-11-16 12:31:59 +0700

    Initial commit

---
+++ src/Generous.java
@@ -0,0 +1,9 @@
+
+public class Generous implements BonusCalculator {
+
+	@Override
+	public double computeBonus(double wage) {
+		return wage*10/100;
+	}
+
+}
---
+++ src/Manager.java
@@ -0,0 +1,7 @@
+
+public class Manager extends Employee {
+	public Manager(double wage) {
+		salary = wage;
+		bc = new Skimpy();
+	}
+}
